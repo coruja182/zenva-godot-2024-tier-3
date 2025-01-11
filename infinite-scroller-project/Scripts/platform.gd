cae746d29f82a74fccc4bc718f41602ec8125719
Luis Henrique Rocha <henrique.srocha@gmail.com>
2025-01-11 21:20:28 +0100

    chore(intinite scroller): create platform using tilemap

---
+++ infinite-scroller-project/Scripts/platform.gd
@@ -0,0 +1,19 @@
+extends TileMapLayer
+class_name Platform
+
+
+var speed: int = 100
+var platform_size: int = 10
+
+
+func _physics_process(delta: float) -> void:
+	position.x -= speed * delta
+
+
+func _ready():
+	# Creating the platform programmatically
+	for i in range(0, platform_size):
+		var l_coords = Vector2i(i, 0)
+		var l_source_id = 1
+		var l_atlas_coords = Vector2i.ZERO
+		set_cell(l_coords, l_source_id, l_atlas_coords)
