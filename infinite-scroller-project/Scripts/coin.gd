bc5fad2a84dfe1c6b679d42a49164b44f071dce8
Luis Henrique Rocha <henrique.srocha@gmail.com>
2025-01-19 20:00:00 +0100

    feat(game): implement score tracking and UI updates
    feat(ui): add score label to display current score
    feat(coin): play sound and update score on collection
    chore(world): update scene to include UI controller

--- infinite-scroller-project/Scripts/coin.gd
+++ infinite-scroller-project/Scripts/coin.gd
@@ -8,19 +8,21 @@ class_name Coin
 
 
 func _ready():
-    audio_stream_player_2d.finished.connect(_on_audio_stream_player_2d_finished)
-    body_entered.connect(_on_body_entered)
-    animation_player.play("spin")
+	audio_stream_player_2d.finished.connect(_on_audio_stream_player_2d_finished)
+	audio_stream_player_2d.volume_db = -10
+	body_entered.connect(_on_body_entered)
+	animation_player.play("spin")
 
 
 func _on_audio_stream_player_2d_finished() -> void:
-    self.queue_free()
+	self.queue_free()
 
 
 func _on_body_entered(_body: Node) -> void:
-    print_debug("Coin %s collected!" % self.name)
-    # check if the body that entered is the player
-    if _body is Player:
-        # disable the collision shape to prevent multiple collections
-        hide()
-        collision_shape_2d.call_deferred("set_disabled", true)
+	# check if the body that entered is the player
+	if _body is Player:
+		audio_stream_player_2d.play()
+		# disable the collision shape to prevent multiple collections
+		hide()
+		collision_shape_2d.call_deferred("set_disabled", true)
+		GameController.increase_score(1)
