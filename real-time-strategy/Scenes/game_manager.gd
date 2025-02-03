847b2235f067e4ed699cedfc8ec922551ba71954
Luís Henrique Rocha <henrique.srocha@gmail.com>
2025-02-03 23:54:31 +0100

    feat(rts-game): WIP game manager get selected units function

--- real-time-strategy/Scenes/game_manager.gd
+++ real-time-strategy/Scenes/game_manager.gd
@@ -7,4 +7,12 @@ var enemies : Array[Unit]
 
 
 func _get_selected_unit():
-	pass
+	var space = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = get_global_mouse_position()
+	var intersection : Array[Dictionary]  = space.intersect_point(query, 1)
+
+	if !intersection.is_empty():
+		return intersection[0].collider
+
+	return null
