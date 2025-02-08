8048ffc35481ef5d6df567a2eb3f0524159b704f
Luis Henrique Rocha <henrique.srocha@gmail.com>
2025-02-08 21:18:00 +0100

    feat(2d platformer): state machine setup

--- 2d-platformer-tutorial/Scripts/player/player.gd
+++ 2d-platformer-tutorial/Scripts/player/player.gd
@@ -2,25 +2,37 @@ extends CharacterBody2D
 class_name PlayerController
 
 
-const SPEED = 300.0
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+
+
+var max_speed: int = 8000
+var acceleration: int = 1000
+var jump_height: int = 15000
+var direction: float = 0.0
+
 const JUMP_VELOCITY = -400.0
+const FRICTION: float = 0.22
+const WEIGHT: float = 0.35
+
+
+# State Machine Related Code #
+var current_state: String =  ""
+var can_dash: bool = true
+var max_jumps: int = 3
+var jump_count: int = 0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		var target_velocity : float = min(velocity.y + acceleration * delta, max_speed)
+		velocity.y = lerp(velocity.y, target_velocity, 0.6)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	direction = Input.get_axis("ui_left", "ui_right")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if direction < 0:
+		animation.flip_h = false
+	elif direction > 0:
+		animation.flip_h = true
 
 	move_and_slide()
