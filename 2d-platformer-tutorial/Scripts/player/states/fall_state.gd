e4172df453a4163160fa0b7de62261dfacc51a45
Luis Henrique Rocha <henrique.srocha@gmail.com>
2025-02-13 19:01:00 +0100

    feat(2d platformer): adjust frame progress and refine fall state logic

--- 2d-platformer-tutorial/Scripts/player/states/fall_state.gd
+++ 2d-platformer-tutorial/Scripts/player/states/fall_state.gd
@@ -2,15 +2,19 @@ extends State
 class_name FallState
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func reset_node() -> void:
 	pass
+
+
+func _physics_process(delta: float) -> void:
+	if player.current_state == "Fall":
+		if Input.is_action_pressed("move_right"):
+			player.move_left(delta)
+		elif Input.is_action_pressed("move_left"):
+			player.move_right(delta)
+
+		if Input.is_action_just_pressed("jump") and (player.jump_count < player.max_jumps):
+			player.jump(delta)
+
+		if player.is_on_floor():
+			player.change_state("Idle")
